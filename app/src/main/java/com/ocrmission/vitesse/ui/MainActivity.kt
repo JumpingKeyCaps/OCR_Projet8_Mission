c94f2ce3ece6b0d40c6fdb27adb3b0d9532d59c2
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-03 18:28:51 +0200

    feat(main): Add Hilt Annotation to the Main Activity (@AndroidEntrypoint) and pre-setup the loaded fragment by default.

--- app/src/main/java/com/ocrmission/vitesse/ui/MainActivity.kt
+++ app/src/main/java/com/ocrmission/vitesse/ui/MainActivity.kt
@@ -1,16 +1,32 @@
-package com.ocrmission.vitesse
+package com.ocrmission.vitesse.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ocrmission.vitesse.R
+import com.ocrmission.vitesse.databinding.ActivityMainBinding
+import com.ocrmission.vitesse.ui.home.HomeFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //get the view
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //load fragment home
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, HomeFragment()).commit()
+
+        // Set up the system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
