735b5b0de2996d1fb90e5c726291223e02a64329
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-03 16:30:41 +0200

    feat(main): Add an Application class where the Dagger components will be generated.

---
+++ app/src/main/java/com/ocrmission/vitesse/VitesseApplication.kt
@@ -0,0 +1,11 @@
+package com.ocrmission.vitesse
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application Class to use with Hilt
+ */
+@HiltAndroidApp
+class VitesseApplication: Application() {
+}
