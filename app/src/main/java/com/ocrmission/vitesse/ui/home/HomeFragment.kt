03465163d4f836c07ece047cc8ea6bcd384b380d
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-03 17:42:22 +0200

    feat(ui/home): Create the Home Fragment.

---
+++ app/src/main/java/com/ocrmission/vitesse/ui/home/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.ocrmission.vitesse.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.ocrmission.vitesse.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
+
+/**
+ * The Home Fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    //TODO: Declare the view model here
+    //private val viewModel: HomeViewModel by viewModels()
+
+    /**
+     * Fragment LifeCycle - called when the fragment is create.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /**
+     * Fragment LifeCycle - called when the fragment was created.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            //TODO: Call the view model method here to collect the flow
+        }
+
+    }
+
+}
