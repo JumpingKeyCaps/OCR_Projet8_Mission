4544ca067d96ba14dfc50300f8760d62652a7471
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-06 16:11:24 +0200

    feat(ui/home): Add the base structure of the HomeViewmodel.

---
+++ app/src/main/java/com/ocrmission/vitesse/ui/home/HomeViewModel.kt
@@ -0,0 +1,29 @@
+package com.ocrmission.vitesse.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ocrmission.vitesse.domain.Candidate
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+/**
+ * The home view model.
+ */
+@HiltViewModel
+class HomeViewModel(): ViewModel() {
+
+    private val _candidates = MutableStateFlow<List<Candidate>>(emptyList())
+    val candidates: StateFlow<List<Candidate>> = _candidates.asStateFlow()
+
+    fun fetchCandidates() {
+        viewModelScope.launch(Dispatchers.IO) {
+
+        }
+    }
+
+
+}
