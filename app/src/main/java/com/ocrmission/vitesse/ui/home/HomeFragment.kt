e09b18f202b5f9af4069d5e0ac94c2bd110fad63
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-06 15:51:27 +0200

    feat(ui/home): Add the Home Fragment, and setup the ViewPager and the tablayout inside.

--- app/src/main/java/com/ocrmission/vitesse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ocrmission/vitesse/ui/home/HomeFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.ocrmission.vitesse.R
 import com.ocrmission.vitesse.databinding.FragmentHomeBinding
+import com.ocrmission.vitesse.ui.home.candidatesList.CandidatesListFragment
+import com.ocrmission.vitesse.ui.home.favoritesList.FavoritesListFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -22,13 +27,12 @@ class HomeFragment : Fragment() {
     /**
      * Fragment LifeCycle - called when the fragment is create.
      */
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView( inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        //Setup the UI
+        SetupUI()
         return binding.root
     }
 
@@ -38,9 +42,60 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch {
-            //TODO: Call the view model method here to collect the flow
+
         }
+    }
+
+    /**
+     * Initialize the UI.
+     */
+    private fun SetupUI(){
+        //init ViewPager
+        setupViewPager()
+        //init TabLayout
+        setupTabLayout()
 
     }
+
+
+    /**
+     * Initialize the view pager.
+     */
+    private fun setupViewPager(){
+        // Create an instance of FragmentStateAdapter with 2 pages
+        val adapter = object : FragmentStateAdapter(childFragmentManager,viewLifecycleOwner.lifecycle) {
+            override fun getItemCount(): Int {
+                return 2 // Nombre de fragments à afficher
+            }
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0 -> CandidatesListFragment() // Fragment for the first page : Candidates List
+                    1 -> FavoritesListFragment() // Fragment for the second page : Favorite List
+                    else -> throw IllegalArgumentException("Invalid fragment position")
+                }
+            }
+        }
+        // Set the adapter for the ViewPager
+        binding.homeViewPager.adapter = adapter
+    }
+
+    /**
+     * Initialize the tab layout.
+     */
+    private fun setupTabLayout(){
+        // Connect TabLayout with ViewPager using TabLayoutMediator
+        val tabLayoutMediator = TabLayoutMediator(binding.tabs, binding.homeViewPager) { tab, position ->
+            // Set tab text or icon based on position
+            when (position) {
+                0 -> { tab.text = getString(R.string.tab_AllCandidat_text)}
+                1 -> { tab.text = getString(R.string.tab_FavCandidat_text)}
+            }
+        }
+        // Attach TabLayoutMediator to the ViewPager
+        tabLayoutMediator.attach()
+    }
+
+
+
 
 }
