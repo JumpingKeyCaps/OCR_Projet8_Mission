9a3833be5a6aff732d311d56d6a31ff8e95df4b7
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-06 16:21:22 +0200

    feat(domaine): Add a Candidate model object.

---
+++ app/src/main/java/com/ocrmission/vitesse/domain/Candidate.kt
@@ -0,0 +1,25 @@
+package com.ocrmission.vitesse.domain
+
+import java.time.LocalDateTime
+
+/**
+ *  class to create Object Candidate who modeling the candidate data.
+ */
+data class Candidate(
+    @JvmField var id: Int,
+    var firstname: String,
+    var lastname: String,
+    var email: String,
+    var phone: String,
+    var birthday: LocalDateTime,
+    var salary: Long,
+    var note: String,
+    var photoUrl: String
+
+) {
+
+
+
+
+
+}
