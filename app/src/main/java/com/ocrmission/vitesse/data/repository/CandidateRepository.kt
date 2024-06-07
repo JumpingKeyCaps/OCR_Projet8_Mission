c2f4ed3fd4fc6a11c5150bd4a68c81085e26d3e6
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-07 13:24:35 +0200

    feat(data/repository): Add all methods inside the Candidate Repository + Kdoc.

--- app/src/main/java/com/ocrmission/vitesse/data/repository/CandidateRepository.kt
+++ app/src/main/java/com/ocrmission/vitesse/data/repository/CandidateRepository.kt
@@ -1,4 +1,48 @@
 package com.ocrmission.vitesse.data.repository
 
-class CandidateRepository {
+import com.ocrmission.vitesse.data.room.dao.CandidateDtoDao
+import com.ocrmission.vitesse.data.room.entity.CandidateDto
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * Repository for Candidate data
+ */
+class CandidateRepository @Inject constructor(private val CandidateDao: CandidateDtoDao) {
+
+    /**
+     * Method to get all candidates.
+     * @return a flow of a list of CandidateDto.
+     */
+    fun getAllCandidates(): Flow<List<CandidateDto>>{
+        return CandidateDao.getAllCandidates()
+    }
+
+    /**
+     * Method to add a candidate.
+     * @param candidate the candidate dto to be added.
+     */
+    suspend fun addCandidate(candidate: CandidateDto){
+        CandidateDao.insertCandidate(candidate)
+
+    }
+
+    /**
+     * Method to delete a candidate.
+     * @param candidate the candidate dto to be deleted.
+     */
+    suspend fun deleteCandidate(candidate: CandidateDto){
+        CandidateDao.deleteCandidate(candidate)
+    }
+
+    /**
+     * Method to update a candidate profile.
+     * @param candidate the candidate dto with updated information.
+     * @return 1 if successful, 0 otherwise.
+     */
+    suspend fun updateCandidate(candidate: CandidateDto): Int {
+        return CandidateDao.updateCandidate(candidate)
+    }
+
+
 }
