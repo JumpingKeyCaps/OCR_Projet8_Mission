0f6c2ddf2880b8df69c76064ae9fc291a5352b1e
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-09 17:45:17 +0200

    feat(domain): - Add the convert methods in Candidate object toDto() and fromDto(). - Change type of id and salary proprieties.

--- app/src/main/java/com/ocrmission/vitesse/domain/Candidate.kt
+++ app/src/main/java/com/ocrmission/vitesse/domain/Candidate.kt
@@ -1,26 +1,67 @@
 package com.ocrmission.vitesse.domain
 
+import com.ocrmission.vitesse.data.room.entity.CandidateDto
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 /**
  *  class to create Object Candidate who modeling the candidate data.
  */
 data class Candidate(
-    @JvmField var id: Int,
+    @JvmField var id: Long,
     var firstname: String,
     var lastname: String,
     var email: String,
     var phone: String,
     var birthday: LocalDateTime,
-    var salary: Long,
+    var salary: Int,
     var note: String,
     var photoUri: String,
     var isFavorite: Boolean
 
 ) {
+    /**
+     *  function to convert Candidate object to CandidateDto object
+     *  @return CandidateDto object
+     */
+    fun toDto(): CandidateDto {
+        return CandidateDto(
+            firstname = this.firstname,
+            lastname = this.lastname,
+            email = this.email,
+            phone = this.phone,
+            birthday = (this.birthday.atZone(ZoneId.systemDefault()).toInstant()).toEpochMilli(),
+            salary = this.salary,
+            note = this.note,
+            photoUri = this.photoUri,
+            isFavorite = this.isFavorite
+        )
+    }
 
+    companion object {
+        /**
+         *  function to convert CandidateDto object to Candidate object
+         *  @param dto CandidateDto object to convert.
+         *  @return Candidate object.
+         */
+        fun fromDto(dto: CandidateDto): Candidate {
+            val instant = Instant.ofEpochMilli(dto.birthday)
+            val localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
+            return Candidate(
+                id = dto.id,
+                firstname = dto.firstname,
+                lastname = dto.lastname,
+                email = dto.email,
+                phone = dto.phone,
+                birthday = localDateTime,
+                salary = dto.salary,
+                note = dto.note,
+                photoUri = dto.photoUri,
+                isFavorite = dto.isFavorite
+            )
+        }
 
-
-
+    }
 
 }
