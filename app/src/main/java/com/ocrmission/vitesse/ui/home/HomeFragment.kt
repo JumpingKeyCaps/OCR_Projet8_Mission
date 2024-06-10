84f1dff27812bf8072b8cf6a7eb5124ebf612354
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-10 15:12:30 +0200

    feat(ui/home): - Add FAB button. - Add custom ItemClickListener Interface + logic. - Link the click to the NavigationUtils object methods calls. - Add reset cross icon on searchbar + logic

--- app/src/main/java/com/ocrmission/vitesse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ocrmission/vitesse/ui/home/HomeFragment.kt
@@ -1,23 +1,24 @@
 package com.ocrmission.vitesse.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ocrmission.vitesse.R
 import com.ocrmission.vitesse.databinding.FragmentHomeBinding
 import com.ocrmission.vitesse.ui.home.candidatesList.CandidatesListFragment
 import com.ocrmission.vitesse.ui.home.favoritesList.FavoritesListFragment
+import com.ocrmission.vitesse.ui.utils.NavigationUtils
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 /**
  * The Home Fragment.
@@ -57,6 +58,8 @@ class HomeFragment : Fragment() {
         setupTabLayout()
         //init SearchBar
         setupSearchBar()
+        //init FAB
+        setupFAB()
 
     }
 
@@ -103,6 +106,7 @@ class HomeFragment : Fragment() {
      * initialize the searchbar
      *
      */
+    @SuppressLint("ClickableViewAccessibility")
     private fun setupSearchBar(){
         binding.searchInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -115,6 +119,16 @@ class HomeFragment : Fragment() {
             }
 
             override fun afterTextChanged(s: Editable?) {
+
+                //change the search icon to a cross to fast reset search.
+                if(s.toString().isEmpty()){
+                    binding.searchIcon.setImageResource(R.drawable.baseline_search_a)
+                }else{
+                    binding.searchIcon.setImageResource(R.drawable.erase_search_b)
+                }
+
+
+
                 // Code à exécuter après que le texte a été modifié
                 val searchQuery = s.toString()
                 Log.d("HomeFragment", "Search query: $searchQuery")
@@ -124,6 +138,53 @@ class HomeFragment : Fragment() {
         })
 
 
+
+
+        //search reset icon listener
+        binding.searchIcon.setOnTouchListener{v, event ->
+            when(event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    v.animate().cancel()
+                    v.scaleX = 0.8f
+                    v.scaleY = 0.8f
+                    return@setOnTouchListener true
+                }
+                MotionEvent.ACTION_UP -> {
+                    v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(200).start()
+                    if(binding.searchInput.text.toString().isNotEmpty()){
+                        binding.searchInput.text.clear()
+                    }
+                    return@setOnTouchListener true
+                }
+                else -> {
+                    v.scaleX = 1.0f
+                    v.scaleY = 1.0f
+                    return@setOnTouchListener false
+                }
+            }
+        }
+
+
     }
+
+    /**
+     * Initialize the FAB.
+     */
+    fun setupFAB(){
+        binding.fab.setOnClickListener {
+            navigateToDetailFragment()
+        }
+    }
+
+
+
+
+    /**
+     * Navigate to the Create fragment, to create a new candidate.
+     */
+    private fun navigateToDetailFragment() {
+        NavigationUtils.navigateToAddCandidateFragment(this)
+    }
+
 
 }
