8f9830f3056e0ae7b3ab0c41728e35e1f90ebd6a
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-24 19:49:22 +0200

    feat(di):  Upgrade the AppModule to add all the retrofit and moshi providers (for Hilt injections).

--- app/src/main/java/com/ocrmission/vitesse/di/AppModule.kt
+++ app/src/main/java/com/ocrmission/vitesse/di/AppModule.kt
@@ -2,10 +2,15 @@ package com.ocrmission.vitesse.di
 
 import android.content.Context
 import androidx.room.Room
+import com.ocrmission.vitesse.data.service.network.interfaces.RetrofitService
+import com.ocrmission.vitesse.data.service.network.interfaces.VitesseNetworkService
+import com.ocrmission.vitesse.data.service.network.VitesseNetworkServiceImpl
 import com.ocrmission.vitesse.data.repository.CandidateRepository
 import com.ocrmission.vitesse.data.room.AppDatabase
 import com.ocrmission.vitesse.data.room.DatabasePrepopulateCallback
 import com.ocrmission.vitesse.data.room.dao.CandidateDtoDao
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,6 +19,8 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 
@@ -24,6 +31,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class AppModule {
 
+    //LOCAL DATA BASE (ROOM)
     @Provides
     @Singleton
     fun provideCoroutineScope(): CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
@@ -51,6 +59,39 @@ class AppModule {
     fun provideCandidateRepository(candidateDtoDao: CandidateDtoDao): CandidateRepository{
         return CandidateRepository(candidateDtoDao)
     }
+
+    //NETWORK (RETROFIT)
+    /**
+     * Moshi instance
+     * build with KotlinJsonAdapterFactory.
+     *
+     */
+    private val moshi: Moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    /**
+     * Method to provide the Retrofit instance to use.
+     *
+     * @return a retrofit instance ready to use with the good URL and parser.
+     */
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(AppConfig.baseCurrencyConverter_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideVitesseNetworkService(retrofit: Retrofit): VitesseNetworkService {
+        val retrofitService = retrofit.create(RetrofitService::class.java)
+        return VitesseNetworkServiceImpl(retrofitService)
+    }
+
 
 
 
