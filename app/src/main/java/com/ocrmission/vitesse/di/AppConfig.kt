96ae38703507368cb82efbee704904ba9f3dc981
Samuel Lebailly <sam.meusa@gmail.com>
2024-06-24 19:34:28 +0200

    feat(di): Add the AppConfig object to manage all the App URL + add the target url of the currency converter API.

---
+++ app/src/main/java/com/ocrmission/vitesse/di/AppConfig.kt
@@ -0,0 +1,8 @@
+package com.ocrmission.vitesse.di
+/**
+ * Object App Config to easy use of all URL in app
+ */
+object AppConfig {
+    /** the currency converter API server URL */
+    const val baseCurrencyConverter_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/"
+}
