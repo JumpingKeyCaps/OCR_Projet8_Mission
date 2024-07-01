54b78be87a307249984ca4c698df19df868eb750
Samuel Lebailly <sam.meusa@gmail.com>
2024-07-01 21:03:55 +0200

    feat(domain): Add getEuroRateFor() method to extract the euro -> target devise value from the response.

--- app/src/main/java/com/ocrmission/vitesse/domain/CurrencyResponse.kt
+++ app/src/main/java/com/ocrmission/vitesse/domain/CurrencyResponse.kt
@@ -1,7 +1,18 @@
 package com.ocrmission.vitesse.domain
 
+
 /**
  * Data class model of the currency converter response.
  * @param eur the map of all the currency rate for the given currency
  */
-data class CurrencyResponse(val eur: Map<String, Double>)
+data class CurrencyResponse(val eur: Map<String, Double>){
+    /**
+     * Get the EURO currency rate for the given currency code.
+     * @param currencyCode the target currency code
+     * @return the EURO currency rate for the given currency code
+     */
+    fun getEuroRateFor(currencyCode: String): Double? {
+        return eur[currencyCode]
+    }
+
+}
