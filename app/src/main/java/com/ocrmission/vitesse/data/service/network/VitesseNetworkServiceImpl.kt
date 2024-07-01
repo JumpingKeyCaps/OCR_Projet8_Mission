a2e0815ab5a7451949eaf08c71a683cd744fa687
Samuel Lebailly <sam.meusa@gmail.com>
2024-07-01 20:57:10 +0200

    feat(data/service/network): Upgrade the way to get the Euro rate via the getConversionRateEurTo() method + add custom network exceptions.

--- app/src/main/java/com/ocrmission/vitesse/data/service/network/VitesseNetworkServiceImpl.kt
+++ app/src/main/java/com/ocrmission/vitesse/data/service/network/VitesseNetworkServiceImpl.kt
@@ -2,6 +2,8 @@ package com.ocrmission.vitesse.data.service.network
 
 import com.ocrmission.vitesse.data.service.network.interfaces.RetrofitService
 import com.ocrmission.vitesse.data.service.network.interfaces.VitesseNetworkService
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 /**
@@ -14,22 +16,41 @@ class VitesseNetworkServiceImpl @Inject constructor(private val retrofitService:
 
 
     /**
-     * Fetches conversion rates for all currencies relative to the provided currency.
-     *
-     * @param currency The currency code to convert from (e.g., "USD").
-     * @return A map containing conversion rates for all currencies relative to the provided currency.
-     * The key is the currency code and the value is the conversion rate.
+     * Fetches conversion rates for EURO to the given target currency.
+     * @param targetCurrency The target currency for which conversion rates are fetched.
+     * @return the conversion rates for EUR to the given target currency.
      * @throws Exception If an error occurs during the network request or data parsing.
      */
-    override suspend fun getConversionRate(currency: String): Map<String, Double> {
+    override suspend fun getConversionRateEurTo(targetCurrency: String): Double? {
         try {
-
-            val response = retrofitService.getConversionRate(currency)
-            return response.eur  // Access the rate map from the response
-
-
+            //Get the conversion rate
+            val response = retrofitService.getConversionRate("eur")
+            //check if the response is successful
+            if(response.isSuccessful){
+                //extract the eur-gbp conversion rate from the response body
+                return response.body()?.getEuroRateFor(targetCurrency)
+            }else{
+                //reponse is not successful - throw a custom exception
+                throw NetworkException.ServerErrorException(response.code())
+            }
         } catch (e: Exception) {
-            throw Exception("Error $e")
+            //handle network/others exceptions
+            throw customNetworkException(e)
+        }
+    }
+
+
+    /**
+     * Methode to throw a custom NetworkException based on the type of exceptions.
+     *
+     * @param e the raised exception.
+     * @return  Throwable custom network exception.
+     */
+    private fun customNetworkException(e:Exception): Throwable{
+        return when (e) {
+            is SocketTimeoutException -> NetworkException.NetworkConnectionException(isSocketTimeout = true,isConnectFail = false)
+            is ConnectException -> NetworkException.NetworkConnectionException(isSocketTimeout = false,isConnectFail = true)
+            else -> NetworkException.UnknownNetworkException
         }
     }
 
